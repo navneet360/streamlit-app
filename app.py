66b32e2d8551121f59316335e7fe770b175babf2
NavaneethaKrishnan <navneet360@gmail.com>
2024-05-23 21:28:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,14 +61,14 @@ if submit_button:
         npv, price, fcf, terminal_value, revised_fcf, pv = calculate_cash_flows(sales, growth_rate, growth_rate_decline, ebit_margin, increase_in_nwc, capex, dep_exp, cash, debt, os_shares, tax_rate, wacc, start_year, end_year)
         st.success("Free Cash Flow (FCF):")
         for item in fcf:
-            st.write(f" - ${item,.2f}")
+            st.write(f" - ${item:,.2f}")
         st.success(f"Terminal Value: ${terminal_value}")
         st.success("Total Free Cash Flow:")
         for item in revised_fcf:
-            st.write(f" - ${item,.2f}")
+            st.write(f" - ${item:,.2f}")
         st.success("Present Value of Free Cash Flow (FCF):")
         for item in pv:
-            st.write(f" - ${item,.2f}")            
+            st.write(f" - ${item:,.2f}")            
         st.success(f'The NPV is: ${npv:,.2f}')
         st.success(f'The Price per Share is: ${price:,.2f}')            
     except Exception as e:
